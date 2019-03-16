52da64a9d179d09c839a5853bb07b2a1d6e25990
SyStem-5 <g1-assassin@hotmail.com>
2019-03-16 16:20:28 +0100

    Updated build script for mosquitto auth docker image. Cleaned up and truncated the copy commands.

--- build.sh
+++ build.sh
@@ -9,34 +9,45 @@ mkdir -p $build_dir
 
 rsync -a --info=progress2 source/neutron_updateserver $build_dir
 rsync -a --info=progress2 source/ufw_setup $build_dir
+
 rsync -a --info=progress2 source/mosquitto $build_dir
+git clone https://github.com/SyStem-5/Mosquitto-Auth-DockerImage.git $build_dir/mosquitto/mosquitto_docker
 
 rsync --info=progress2 source/install.sh $build_dir
 
 rsync --info=progress2 source/docker_run_neutron_updateserver.sh $build_dir
 
 
-rsync --info=progress2 \
-    ../NeutronServer/Pipfile $build_dir/neutron_updateserver/django/
+# rsync --info=progress2 \
+#     ../NeutronServer/Pipfile $build_dir/neutron_updateserver/django/
 
-rsync --info=progress2 \
-    ../NeutronServer/Pipfile.lock $build_dir/neutron_updateserver/django/
+# rsync --info=progress2 \
+#     ../NeutronServer/Pipfile.lock $build_dir/neutron_updateserver/django/
 
-rsync --info=progress2 \
-    ../NeutronServer/manage.py $build_dir/neutron_updateserver/django/app/
+# rsync --info=progress2 \
+#     ../NeutronServer/manage.py $build_dir/neutron_updateserver/django/app/
 
-rsync -a --info=progress2 \
-    ../NeutronServer/NeutronServer $build_dir/neutron_updateserver/django/app/ \
-    --exclude __pycache__
+# rsync -a --info=progress2 \
+#     ../NeutronServer/NeutronServer $build_dir/neutron_updateserver/django/app/ \
+#     --exclude __pycache__
 
-rsync -a --info=progress2 \
-    ../NeutronServer/static $build_dir/neutron_updateserver/django/app/
+# rsync -a --info=progress2 \
+#     ../NeutronServer/static $build_dir/neutron_updateserver/django/app/
 
-rsync -a --info=progress2 \
-    ../NeutronServer/templates $build_dir/neutron_updateserver/django/app/
+# rsync -a --info=progress2 \
+#     ../NeutronServer/templates $build_dir/neutron_updateserver/django/app/
 
-rsync -a --info=progress2 \
-    ../NeutronServer/update_manager $build_dir/neutron_updateserver/django/app/
+# rsync -a --info=progress2 \
+#     ../NeutronServer/update_manager $build_dir/neutron_updateserver/django/app/
+
+rsync -a --info=progress2 ../NeutronServer/ $build_dir/neutron_updateserver/django/app/ \
+    --exclude .vscode \
+    --exclude .git \
+    --exclude __pycache__ \
+    --exclude README.md \
+    --exclude run_dev_server.sh \
+    --exclude set_dev_env_vars.sh \
+    --exclude .gitignore
 
 # rsync -a --info=progress2 \
 #     ../NeutronServer/version_control $build_dir/neutron_updateserver/django/app/
