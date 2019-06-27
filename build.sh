1cfcba59211aca183e867b041e80b8722fb52895
SyStem-5 <g1-assassin@hotmail.com>
2019-06-27 15:18:51 +0200

    Build file cleanup

--- build.sh
+++ build.sh
@@ -2,14 +2,25 @@
 
 build_dir=build/NeutronUpdateServerInstaller
 
+# Try to remove the build directory
 rm -rf $build_dir
 
+# Create the build directory
 mkdir -p $build_dir
 
+# Base install script
+rsync --info=progress2 source/install.sh $build_dir
 
-rsync -a --info=progress2 source/neutron_updateserver $build_dir
+# Firewall setup script
 rsync -a --info=progress2 source/ufw_setup $build_dir
 
+# Docker run command script
+rsync --info=progress2 source/docker_run_neutron_updateserver.sh $build_dir
+
+# Copy the docker files for the webapp
+rsync -a --info=progress2 source/neutron_updateserver $build_dir
+
+# Mosquitto
 rsync -a --info=progress2 source/mosquitto $build_dir
 rsync -a --info=progress2 ../Mosquitto-Auth-DockerImage/ $build_dir/mosquitto/mosquitto_docker \
     --exclude .vscode \
@@ -23,10 +34,7 @@ rsync -a --info=progress2 ../Mosquitto-Auth-Plugin/ $build_dir/mosquitto/mosquit
     --exclude .gitignore \
     --exclude .gitmodules
 
-rsync --info=progress2 source/install.sh $build_dir
-
-rsync --info=progress2 source/docker_run_neutron_updateserver.sh $build_dir
-
+# Copy the webapp files
 rsync -a --info=progress2 ../NeutronServer/ $build_dir/neutron_updateserver/django/app/ \
     --exclude .vscode \
     --exclude .git \
@@ -35,6 +43,3 @@ rsync -a --info=progress2 ../NeutronServer/ $build_dir/neutron_updateserver/djan
     --exclude run_dev_server.sh \
     --exclude set_dev_env_vars.sh \
     --exclude .gitignore
-
-# rsync -a --info=progress2 \
-#     ../NeutronServer/version_control $build_dir/neutron_updateserver/django/app/
